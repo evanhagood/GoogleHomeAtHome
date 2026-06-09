e2baad25fd155dbbf2553964667910d6e9a362ae
evanhagood <evanmhagood@gmail.com>
2026-06-09 13:33:33 -0400

    Spring Boot Initialzr Init Commit

---
+++ src/main/java/com/evanhagood/GoogleAtHome/GoogleAtHomeApplication.java
@@ -0,0 +1,13 @@
+package com.evanhagood.GoogleAtHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAtHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAtHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evanhagood/GoogleAtHome/GoogleAtHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evanhagood.GoogleAtHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAtHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
